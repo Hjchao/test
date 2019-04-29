22e2233854f4600bdc5398f1daba1c48c6b3c3d7
胡金超 <1120217274@qq.com>
2019-04-29 17:26:15 +0800

    ui自动化

---
+++ TestCase/test_jiujiu.py
@@ -0,0 +1,16 @@
+
+
+
+def test_jiujiu():
+
+
+    for i in range(1,10):
+        x=i+1
+        for j in range(1,x):
+            print('%s*%s=%s'(i,j,i*j),end='')
+    print('')
+
+if __name__ == '__main__':
+    test_jiujiu()
+
+
